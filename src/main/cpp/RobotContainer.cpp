29a4bbe9c07ab9bfbcce05fc862ab35f1789a9b7
John <john@localhost>
2021-01-16 13:56:22 -0500

    Revert "yayyy"
    This reverts commit 7c525db07bae06588f50a928758a476cd65027a4.

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -3,27 +3,27 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "RobotContainer.h"
-#include "subsystems/DriveSubsystem.h"
+
 
 using namespace frc;
-using namespace std;
-RobotContainer::RobotContainer() : m_autonomousCommand(*&m_subsystem) {
+
+RobotContainer::RobotContainer() : m_autonomousCommand(&m_subsystem) {
   // Initialize all of your commands and subsystems here
   Controller1 = new XboxController(0);
-  Controller2 = new XboxController(1);
+  Controller2 = new XboxController(0);
   
-  //m_subsystem = new DriveSubsystem();
+  m_subsystem = new DriveSubsystem();
 
   // Configure the button bindings
   ConfigureButtonBindings();
-  cout<<"Gamer";
-  m_subsystem->SetDefaultCommand(RunCommand(
+
+  m_subsystem.SetDefaultCommand(frc2::RunCommand(
       [this] {
-        m_subsystem->TankDriveVolts(
+        m_subsystem.TankDriveVolts(
             Controller1->GetY(frc::GenericHID::kLeftHand),
             Controller2->GetX(frc::GenericHID::kRightHand));
       },
-      {&m_Drivesubsystem}));
+      {&m_subsystem}));
 }
 
 void RobotContainer::ConfigureButtonBindings() {
--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -39,16 +39,10 @@ class RobotContainer {
 
  private:
   // The robot's subsystems and commands are defined here...
-  DriveSubsystem m_Drivesubsystem;
+  DriveSubsystem m_subsystem;
   ExampleCommand m_autonomousCommand;
   XboxController *Controller1;
   XboxController *Controller2;
   //hi I am Stupid (Teddy)
-
-
-  // frc2::InstantCommand m_driveHalfSpeed{[this] { m_subsystem.SetMaxOutput(0.5); },
-  //                                       {}};
-  // frc2::InstantCommand m_driveFullSpeed{[this] { m_subsystem.SetMaxOutput(1); },
-  //                                       {}};
   void ConfigureButtonBindings();
 };
